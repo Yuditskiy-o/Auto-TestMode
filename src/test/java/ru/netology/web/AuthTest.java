533abd303572057a544c9de96803edcb190cb859
Yuditskiy-o <Komekadze@gmail.com>
2020-09-20 19:42:32 +0700

    Added logic to Generation class and tests to check it

--- src/test/java/ru/netology/web/AuthTest.java
+++ src/test/java/ru/netology/web/AuthTest.java
@@ -1,4 +1,56 @@
 package ru.netology.web;
 
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static org.openqa.selenium.By.cssSelector;
+
 public class AuthTest {
+    @Test
+    void shouldSubmitRequestIfUserIsValid() {
+        Registration user = Generation.generateNewValidActiveUser();
+        open("http://localhost:9999");
+        SelenideElement form = $("[action]");
+        form.$(cssSelector("[name=login]")).sendKeys(user.getLogin());
+        form.$(cssSelector("[name=password]")).sendKeys(user.getPassword());
+        form.$(cssSelector("[type=button]")).click();
+        $(byText("Личный кабинет")).waitUntil(Condition.visible, 15000);
+    }
+
+    @Test
+    void shouldNotSubmitRequestIfStatusIsBlocked() {
+        Registration user = Generation.generateNewValidBlockedUser();
+        open("http://localhost:9999");
+        SelenideElement form = $("[action]");
+        form.$(cssSelector("[name=login]")).sendKeys(user.getLogin());
+        form.$(cssSelector("[name=password]")).sendKeys(user.getPassword());
+        form.$(cssSelector("[type=button]")).click();
+        $(byText("Ошибка")).waitUntil(Condition.visible, 15000);
+    }
+
+    @Test
+    void shouldNotSubmitRequestIfLoginIsInvalid() {
+        Registration user = Generation.generateNewUserWithInvalidLogin();
+        open("http://localhost:9999");
+        SelenideElement form = $("[action]");
+        form.$(cssSelector("[name=login]")).sendKeys(user.getLogin());
+        form.$(cssSelector("[name=password]")).sendKeys(user.getPassword());
+        form.$(cssSelector("[type=button]")).click();
+        $(byText("Ошибка")).waitUntil(Condition.visible, 15000);
+    }
+
+    @Test
+    void shouldNotSubmitRequestIfPasswordIsInvalid() {
+        Registration user = Generation.generateNewUserWithInvalidPassword();
+        open("http://localhost:9999");
+        SelenideElement form = $("[action]");
+        form.$(cssSelector("[name=login]")).sendKeys(user.getLogin());
+        form.$(cssSelector("[name=password]")).sendKeys(user.getPassword());
+        form.$(cssSelector("[type=button]")).click();
+        $(byText("Ошибка")).waitUntil(Condition.visible, 15000);
+    }
 }
